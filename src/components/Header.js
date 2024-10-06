388bcad05e97c2a79cfbc31f80773f6c4b0aac6e
Abhishek <bhardwajabhishek140@gmail.com>
2024-10-06 20:46:15 +0530

    firebase setup , signin signup  validateuser ,routes, header RTK-appstore

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,50 @@
 import React from "react";
+import { signOut } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { removeUser } from "../utils/userSlice";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const user = useSelector((store) => store.user);
+  //   const dispatch = useDispatch();
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        // dispatch(removeUser())
+        navigate("/");
+        // Sign-out successful.
+      })
+      .catch((error) => {
+        navigate("/error");
+        // An error happened.
+      });
+  };
   return (
-    <div className=" absolute px-8 py-2 bg-gradient-to-b from-black">
+    <div className=" absolute w-screen px-40 py-2  bg-gradient-to-b from-black z-10 flex justify-between">
       <img
-        className="w-44 "
+        className="w-48 "
         alt="Netflix logo"
         src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
       />
+      {user && (
+        <div className="p-2 mt-2 flex  ">
+          <img
+            alt="user logo"
+            className="w-12 h-12 rounded-md  "
+            src="https://occ-0-6246-2186.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABXz4LMjJFidX8MxhZ6qro8PBTjmHbxlaLAbk45W1DXbKsAIOwyHQPiMAuUnF1G24CLi7InJHK4Ge4jkXul1xIW49Dr5S7fc.png?r=e6e"
+          />
+          <div className="mb-3">
+            <button
+              onClick={handleSignOut}
+              className="m-3 bg-red-600 rounded-md p-1 text-white font-semibold pl-2 pr-2 pb-2 "
+            >
+              Sign Out
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
